3b9e415d263561c1bb1e54cf8ebbb23a1f014958
prithivirajasingh <prithivirajasingh@gmail.com>
2021-07-01 16:36:02 -0700

    Create create_ffmpeg_concat_file.sh

---
+++ create_ffmpeg_concat_file.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# read -p "Enter any key to continue, "a" to abort: " userinput
+if [ "$userinput" = "a" ]; then
+	printf "Program terminated by user."
+	exit
+fi
+if [ "$userinput" = "A" ]; then
+	printf "Program terminated by user."
+	exit
+fi
+
+printf "Task started.\n"
+
+read -p "Enter file extension with dot (Eg. .mp4): " fileextension
+if [ -z "$filecount" ]; then echo "Set to default extension .mp4" && fileextension=".mp4"; fi
+
+read -p "Enter filecount: " filecount
+if [ -z "$filecount" ]; then echo "Input cannot be empty. Program will now exit." & exit; fi
+
+
+if [ "$filecount" -ge "1000" ]; then
+	echo "Out of range. Program will now exit."
+	exit
+fi
+
+echo "file '0001$fileextension'" > "`pwd`/list.txt"
+
+for (( i=2; i<=$filecount; i++ )); do
+	if [ "$i" -le "9" ]; then
+		echo "file '000$i$fileextension'" >> "`pwd`/list.txt"
+		continue
+	fi
+	if [ "$i" -le "99" ]; then
+		echo "file '00$i$fileextension'" >> "`pwd`/list.txt"
+		continue
+	fi
+	if [ "$i" -le "999" ]; then
+		echo "file '0$i$fileextension'" >> "`pwd`/list.txt"
+		continue
+	fi
+done
+
+printf "Task completed."

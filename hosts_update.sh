cdf56363c7aed332178d0abe92f175f88b29236f
prithivirajasingh <prithivirajasingh@gmail.com>
2021-10-15 16:19:27 -0700

    Create hosts_update.sh

---
+++ hosts_update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/prithivi/bin/hosts/
+curl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts > /home/prithivi/bin/hosts/hosts_master.txt   
+curl https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts > /home/prithivi/bin/hosts/hosts_alternate.txt
+rm /home/prithivi/bin/hosts/hosts
+cat /home/prithivi/bin/hosts/hosts.bak /home/prithivi/bin/hosts/hosts_master.txt /home/prithivi/bin/hosts/hosts_alternate.txt > /home/prithivi/bin/hosts/hosts

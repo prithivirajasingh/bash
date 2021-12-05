b40199bb34cc3eb6512e6c5c0b4e716d757d4734
prithivirajasingh <prithivirajasingh@gmail.com>
2021-12-04 19:34:52 -0800

    Update raspberry_pi_hdmi_cec_tv_remote.sh

--- raspberry_pi_hdmi_cec_tv_remote.sh
+++ raspberry_pi_hdmi_cec_tv_remote.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Location: /home/$USERNAME/bin/raspberry_pi_hdmi_cec_tv_remote.sh
-# Requires installation of: cec-client, xdotool   [Run "sudo apt install cec-utils xdotool"]
-# Execute on login if required. To do so uncomment and add below line in cron_reboot.sh and cron it to run reboot
-# echo password | su $USERNAME -l -c "export DISPLAY=:0; export XAUTHORITY=/home/pi/.Xauthority; /usr/bin/cec-client | /home/$USERNAME/bin/raspberry_pi_hdmi_cec_tv_remote.sh & disown"
+# Location: /home/pi/bin/cec_remote.sh
+# Requires installation of: cec-client, xdotool   [apt install cec-utils xdotool]
+# Execute on login if required. To do so add below line in cron_reboot.sh 
+# echo 6484 | su pi -l -c "export DISPLAY=:0; export XAUTHORITY=/home/pi/.Xauthority; /usr/bin/cec-client | /home/pi/bin/cec_remote.sh & disown"
 
 function keychar {
     parin1=$1 #first param; abc1
@@ -27,6 +27,10 @@ intmsbetweenkeys=1500 #two presses of a key sooner that this makes it delete pre
 intmousestartspeed=5 #mouse starts moving at this speed (pixels per key press)
 intmouseacc=50 #added to the mouse speed for each key press (while holding down key, more key presses are sent from the remote)
 intmousespeed=10
+FILE=~/bin/functionKey  # sub picture key (cc key to the right of 0) is used as functionKey
+if [ -f "$FILE" ]; then
+    rm $FILE
+fi
 
 while read oneline
 do
@@ -56,36 +60,74 @@ do
             strlastkey=$strkey
             case "$strkey" in
                 "1")
-                    keychar "1abcd" intkeychar
+                    if [ -f "$FILE" ]; then
+                        xdotool mousemove 10 10
+                    else
+                        keychar "1abcd" intkeychar
+                    fi
                     ;;
-                    #xdotool key "BackSpace"
-                    #;;
                 "2")
-                    keychar "2efgh" intkeychar
+                    if [ -f "$FILE" ]; then
+                        xdotool mousemove 640 180
+                    else
+                        keychar "2efgh" intkeychar
+                    fi
                     ;;
                 "3")
-                    keychar "3ijkl" intkeychar
+                    if [ -f "$FILE" ]; then
+                        xdotool mousemove 1270 10
+                    else
+                        keychar "3ijkl" intkeychar
+                    fi
                     ;;
                 "4")
-                    keychar "4mnop" intkeychar
+                    if [ -f "$FILE" ]; then
+                        xdotool mousemove 320 360
+                    else
+                        keychar "4mnop" intkeychar
+                    fi
                     ;;
                 "5")
-                    keychar "5qrst" intkeychar
+                    if [ -f "$FILE" ]; then
+                        xdotool mousemove 640 360
+                    else
+                        keychar "5qrst" intkeychar
+                    fi
                     ;;
                 "6")
-                    keychar "6uvw" intkeychar
+                    if [ -f "$FILE" ]; then
+                        xdotool mousemove 960 360
+                    else
+                        keychar "6uvw" intkeychar
+                    fi
                     ;;
                 "7")
-                    keychar "7xyz" intkeychar
+                    if [ -f "$FILE" ]; then
+                        xdotool mousemove 10 710
+                    else
+                        keychar "7xyz" intkeychar
+                    fi
                     ;;
                 "8")
-                    keychar "8" intkeychar
+                    if [ -f "$FILE" ]; then
+                        xdotool mousemove 640 540
+                    else
+                        keychar "8" intkeychar
+                    fi
                     ;;
                 "9")
-                    keychar "9" intkeychar
+                    if [ -f "$FILE" ]; then
+                        xdotool mousemove 1270 710
+                    else
+                        keychar "9" intkeychar
+                    fi
                     ;;
                 "0")
-                    keychar "0 .-" intkeychar
+                    if [ -f "$FILE" ]; then
+                        xdotool key "Ctrl+q"
+                    else
+                        keychar "0 ._-" intkeychar
+                    fi
                     ;;
                 "previous channel")
                     xdotool key "Return" #Enter
@@ -100,28 +142,48 @@ do
                     xdotool click 3 #right mouse button click"
                     ;;
                 "up")
-                    intpixels=$((-1 * intmousespeed))
-                    xdotool mousemove_relative -- 0 $intpixels #move mouse up
-                    intmousespeed=$((intmousespeed + intmouseacc)) #speed up
+                    if [ -f "$FILE" ]; then
+                        xdotool key "Up"
+                    else
+                        intpixels=$((-1 * intmousespeed))
+                        xdotool mousemove_relative -- 0 $intpixels #move mouse up
+                        intmousespeed=$((intmousespeed + intmouseacc)) #speed up
+                    fi
                     ;;
                 "down")
-                    intpixels=$(( 1 * intmousespeed))
-                    xdotool mousemove_relative -- 0 $intpixels #move mouse down
-                    intmousespeed=$((intmousespeed + intmouseacc)) #speed up
+                    if [ -f "$FILE" ]; then
+                        xdotool key "Down"
+                    else
+                        intpixels=$(( 1 * intmousespeed))
+                        xdotool mousemove_relative -- 0 $intpixels #move mouse down
+                        intmousespeed=$((intmousespeed + intmouseacc)) #speed up
+                    fi
                     ;;
                 "left")
-                    intpixels=$((-1 * intmousespeed))
-                    xdotool mousemove_relative -- $intpixels 0 #move mouse left
-                    intmousespeed=$((intmousespeed + intmouseacc)) #speed up
+                    if [ -f "$FILE" ]; then
+                        xdotool key "Left"
+                    else
+                        intpixels=$((-1 * intmousespeed))
+                        xdotool mousemove_relative -- $intpixels 0 #move mouse left
+                        intmousespeed=$((intmousespeed + intmouseacc)) #speed up
+                    fi
                     ;;
                 "right")
-                    intpixels=$(( 1 * intmousespeed))
-                    xdotool mousemove_relative -- $intpixels 0 #move mouse right
-                    intmousespeed=$((intmousespeed + intmouseacc)) #speed up
+                    if [ -f "$FILE" ]; then
+                        xdotool key "Right"
+                    else
+                        intpixels=$(( 1 * intmousespeed))
+                        xdotool mousemove_relative -- $intpixels 0 #move mouse right
+                        intmousespeed=$((intmousespeed + intmouseacc)) #speed up
+                    fi
                     ;;
                 "select")
                     #echo "select executed --- debug select"
-                    xdotool click 1 #left mouse button click
+                    if [ -f "$FILE" ]; then
+                        xdotool key "Return"
+                    else
+                        xdotool click 1 #left mouse button click
+                    fi
                     ;;
                 "return")
                     xdotool key "Alt_L+Left" #WWW-Back
@@ -130,26 +192,38 @@ do
                     #echo Key Pressed: EXIT
                     tempstr=`xdotool getactivewindow getwindowname`
                     if [[ "$tempstr" == *"Mozilla Firefox"* ]]; then
-                    	xdotool key "Alt_L+Left"
+                        xdotool key "Alt_L+Left"
                     else
                         xdotool key "BackSpace"
                     fi
                     ;;
                 "F2")
                     # Red in Hisense
-                    xdotool key "Escape"
+                    if [ -f "$FILE" ]; then
+                        xdotool key "f"
+                    else
+                        xdotool key "Escape"
+                    fi
                     #firefox "https://www.tamilhdbox.com/" & disown
                     # echo Key Pressed: RED
                     ;;
                 "F3")
                     # Green in Hisense
-                    xdotool key Page_Up
+                    if [ -f "$FILE" ]; then
+                        pcmanfm ~/bin & disown
+                    else
+                        xdotool key Page_Up
+                    fi
                     #firefox "https://www.1tamilmv.bar/" & disown
                     # echo Key Pressed: GREEN
                     ;;
                 "F4")
                     # Yellow in Hisense
-                    xdotool key Page_Down
+                    if [ -f "$FILE" ]; then
+                        pcmanfm ~/Downloads/ftp & disown
+                    else
+                        xdotool key Page_Down
+                    fi
                     #firefox "https://www.google.com/" & disown
                     # echo Key Pressed: YELLOW
                     ;;
@@ -180,15 +254,22 @@ do
                     ## with my remote I only got "STOP" as key released (auto-released), not as key pressed; see below
                     #echo Key Pressed: STOP
                     ;;
-                ".")
-                    firefox "https://www.tamilhdbox.com/" & disown
+                "sub picture")
+                    touch $FILE
+                    #echo Inside dot pressed. touch command executed.
+                    #firefox "https://www.1tamilmv.bar/" & disown
                     #echo Dot key pressed --- debug
                     #xdotool key Page_Down
                     ## with my remote I only got "STOP" as key released (auto-released), not as key pressed; see below
                     #echo Key Pressed: STOP
                     ;;
-                "sub picture")
-                    firefox "https://www.1tamilmv.bar/" & disown
+                ".")
+                    if [ -f "$FILE" ]; then
+                        firefox "https://www.tamilhdbox.com/" & disown
+                    else
+                        firefox "https://www.1tamilmv.bar/" & disown
+                    fi
+                    #firefox "https://www.1tamilmv.bar/" & disown
                     #echo sub picture key pressed --- debug
                     #xdotool key Page_Up
                     ## with my remote I only got "STOP" as key released (auto-released), not as key pressed; see below
@@ -198,9 +279,9 @@ do
                     #echo Key Pressed: sound select
                     activewindowname=`xdotool getactivewindow getwindowname`
                     if [[ "$activewindowname" == "pcmanfm" ]]; then
-                    	true
+                        true
                         lxde-pi-shutdown-helper & disown
-                    	#echo "No active window to kill"
+                        #echo "No active window to kill"
                     else
                         xdotool getactivewindow windowkill
                     fi
@@ -229,7 +310,12 @@ do
                 "right")
                     intmousespeed=$intmousestartspeed #reset mouse speed
                     ;;
+                "sub picture")
+                    sleep 2 && rm $FILE &
+                    #echo Inside dot released. && sleep 2 && rm $FILE && echo rm command completed &
+                    ;;
             esac
         fi
     fi
 done
+
